aa28ea459c287229bffb74be864c7d79403cccdb
wildbook7 <wildbook7@gmail.com>
2024-10-15 16:05:21 +0900

    GitHub ログインの実装

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { Noto_Sans_JP } from "next/font/google";
 import "./globals.css";
 import Header from "./components/Header";
+import { NextAuthProvider } from "./lib/next-auth/provider";
 
 const notoSansJP = Noto_Sans_JP({
   subsets: ["latin"],
@@ -21,8 +22,10 @@ export default function RootLayout({
   return (
     <html lang="ja">
       <body className={`${notoSansJP.className}  antialiased`}>
-        <Header />
-        {children}
+        <NextAuthProvider>
+          <Header />
+          {children}
+        </NextAuthProvider>
       </body>
     </html>
   );
